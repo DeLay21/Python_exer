3a00a36632face9988b64932146da91584cf06de
Isaac <zazaq32@gmail.com>
2025-10-28 17:44:19 -0300

    exclusão do arquivo .md e exercicio de POO

---
+++ Class_jogador.py
@@ -0,0 +1,20 @@
+class jogador:
+    def __init__(self, nome, pontuacao= 0):
+        self.nome = nome
+        self.pontuacao = pontuacao
+
+    def pontuar(self, ponto= 1):
+        self.pontuacao += ponto
+    
+    def mostrar_ponto(self):
+        print(f"O jogador {self.nome} tem {self.pontuacao}")
+
+jogador1 = jogador("Isaac")
+jogador2 = jogador("arthur")
+
+jogador1.pontuar(2)
+jogador2.pontuar(3)
+
+
+jogador1.mostrar_ponto()
+jogador2.mostrar_ponto()
--- Texto_Excicios_Pyhton.md
+++
@@ -1,3 +0,0 @@
-# Trajetória de Aprendizado em Python
-
-Na minha trajetória de aprendizado sobre a linguagem Python, estes são os exercícios que realizei até agora:
